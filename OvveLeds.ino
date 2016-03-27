afcbe8f85e55c61a57535f229b49d82fc0bd35bf
Jesper Larsson <Jesper Larsson>
2016-03-27 22:39:40 +0200

    setup() and toggle between modes.
    Initialising code in setup() is complete for now. It initiliase the strips
    and the switch.
    
    Some logic for switching between modes is complete.
    
    To be done is the actual animation code.

--- OvveLeds.ino
+++ OvveLeds.ino
@@ -1,20 +1,50 @@
 #include <Adafruit_NeoPixel.h>
 
+//IO config
 #define STRIP1_PIN 0
 #define STRIP2_PIN 1
 #define SWITCH_PIN 2
+#define SWITCH_PRESSED LOW
 
+//Different animation modes
 #define MODE_COLOR_CIRCLE 0
 #define MODE_STATIC_WINERED 666 
 
+//mode - referes to the current animation mode
 int mode = MODE_STATIC_WINERED;
 
+//Strips
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(15, STRIP1_PIN);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(15, STRIP2_PIN);
+
+
 void setup() {
-  // put your setup code here, to run once:
-  
+  //Initialise the strips
+  strip1.begin();
+  strip2.begin();
+  strip1.show();
+  strip2.show();
+
+  //Setup the SWITCH_PIN as an INPUT
+  pinMode(SWITCH_PIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(digitalRead(SWITCH_PIN) == SWITCH_PRESSED) {
+    toggleMode();
+  }
 
+  
 }
+
+void toggleMode() {
+  switch(mode) {
+    case MODE_COLOR_CIRCLE:
+      mode = MODE_STATIC_WINERED;
+      break;
+    case MODE_STATIC_WINERED:
+      mode = MODE_COLOR_CIRCLE;
+      break;
+  }
+}
+
