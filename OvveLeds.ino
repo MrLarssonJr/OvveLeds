af149eab97e836e8cfb8dffc554fcb014d556ed8
Jesper Larsson <Jesper Larsson>
2016-03-27 22:24:38 +0200

    Started laying out some ground work for the strips.

---
+++ OvveLeds.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_NeoPixel.h>
+
+#define STRIP1_PIN 0
+#define STRIP2_PIN 1
+#define SWITCH_PIN 2
+
+#define MODE_COLOR_CIRCLE 0
+#define MODE_STATIC_WINERED 666 
+
+int mode = MODE_STATIC_WINERED;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
